12a0238b9bcd3e27ac993092348bdc47fa8d6678
Jenrinaldo <jenrinaldo@student.ub.ac.id>
2019-05-28 22:28:10 +0700

    update

--- switcher.ino
+++ switcher.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+Servo servoAirTawar;
+Servo servoAirLaut;
+Servo servoAirKeluar;
+ 
+int servoAirTawarPos = 0;
+int servoAirLautPos = 0;
+int servoAirKeluarPos = 0;
+
+void setup() {
+  servoAirTawar.attach (9);
+  servoAirLaut.attach (10);
+  servoAirKeluar.attach (11);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    servoAirTawar.write(120);
+    delay(1000);
+    servoAirTawar.write(0);
 }
